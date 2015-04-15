5cdddf1a84f28f2761a23679193f414b01837081
Andres de Barbara <adebarbara@despegar.com>
2015-04-15 13:10:58 -0300

    Enable configfile

--- src/khronus-collector.go
+++ src/khronus-collector.go
@@ -4,10 +4,12 @@ import (
 	"collector"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"strings"
 
 	"github.com/VividCortex/godaemon"
 	"github.com/codegangsta/cli"
@@ -65,7 +67,20 @@ func main() {
 			}()
 		}
 
-		if c.String("config") != "" {
+		if c.String("configfile") != "" {
+			content, err := ioutil.ReadFile(c.String("configfile"))
+			if err != nil {
+				log.Println("Error loading configfile, continue with default")
+			}
+
+			err = json.Unmarshal([]byte(strings.Join(strings.Split(string(content), "\n"), " ")), &config)
+
+			if err != nil {
+				fmt.Printf("Error loading configuration : %s\n", err)
+			} else {
+				fmt.Println("Configuration loaded")
+			}
+		} else if c.String("config") != "" {
 			err := json.Unmarshal([]byte(c.String("config")), &config)
 
 			if err != nil {
@@ -107,11 +122,11 @@ func main() {
 			Usage: "Show config json available",
 		},
 
-		/*		cli.StringFlag{
-				Name:  "configfile",
-				Value: "",
-				Usage: "Path to json formated configfile",
-			},*/
+		cli.StringFlag{
+			Name:  "configfile",
+			Value: "",
+			Usage: "Path to json formated configfile",
+		},
 	}
 
 	app.Run(os.Args)
